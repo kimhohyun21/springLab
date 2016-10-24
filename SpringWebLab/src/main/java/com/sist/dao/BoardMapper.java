6376d45097deaa21cf9a250d45731644d001efb1
Kim Ho Hyun <kimhohyun21@gmail.com>
2016-10-24 15:31:03 +0900

    commit

--- SpringWebLab/src/main/java/com/sist/dao/BoardMapper.java
+++ SpringWebLab/src/main/java/com/sist/dao/BoardMapper.java
@@ -4,13 +4,13 @@ import java.util.*;
 import org.apache.ibatis.annotations.Select;
 
 public interface BoardMapper {
-	@Select("SELECT no, name, subject, regdate, hit, group_tab, num"
+	@Select("SELECT no, name, subject, regdate, hit, group_tab, num "
 			+ "FROM (SELECT no, name, subject, regdate, hit, group_tab, ROWNUM as num "
 			+ "FROM (SELECT no, name, subject, regdate, hit, group_tab "
 			+ "FROM board ORDER BY group_id DESC, group_step ASC))"
 			+ "WHERE num BETWEEN #{start} AND #{end}")
 	public List<BoardVO> boardListData(Map map);
 	
-	@Select("SELECT CEIL(COUNT(*)/10 FROM board")
+	@Select("SELECT CEIL(COUNT(*)/10) FROM board")
 	public int boardTotalPage();
 }
